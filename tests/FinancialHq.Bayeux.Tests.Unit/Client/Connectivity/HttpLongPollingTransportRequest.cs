2ad71c7455ecb86c3359f643da5240f3fddf0cdc
brian_gardner <1761566+briangardner@users.noreply.github.com>
2019-05-06 16:04:27 -0400

    re-enabled tests.

--- tests/FinancialHq.Bayeux.Tests.Unit/Client/Connectivity/HttpLongPollingTransportRequest.cs
+++ tests/FinancialHq.Bayeux.Tests.Unit/Client/Connectivity/HttpLongPollingTransportRequest.cs
@@ -22,74 +22,87 @@ namespace FinancialHq.Bayeux.Tests.Unit.Client.Connectivity
     {
         private const string FakeUrl = "http://localhost/testing123";
 
-        //[Fact]
-        //public async Task Should_Post_To_Url()
-        //{
-        //    var post = MockHttpPost;
-        //        post.Setup(x => x.PostAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
-        //        .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
-        //        {
-        //            Content = new StringContent(MetaResponse.ToString())
-        //        });
+        [Fact]
+        public async Task Should_Post_To_Url()
+        {
+            var handler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(MetaResponse.ToString())
+                })
+                .Verifiable();
+            var client = new HttpClient(handler.Object);
+            var transport = new HttpLongPollingTransport(GetOptions(client), new List<IExtension>(), Policy.NoOpAsync());
+            await transport.Request(new List<BayeuxMessage>(), CancellationToken.None).ConfigureAwait(false);
+            handler.Protected().Verify("SendAsync",
+                Times.Once(),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Post && req.RequestUri.ToString() == FakeUrl),
+                ItExpr.IsAny<CancellationToken>());
+        }
 
-        //    var transport = new HttpLongPollingTransport(GetOptions(post.Object), new List<IExtension>());
-        //    await transport.Request(new List<BayeuxMessage>(), CancellationToken.None).ConfigureAwait(false);
-        //    post.Verify(x => x.PostAsync(FakeUrl, It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Once);
-        //}
+        [Fact]
+        public async Task Should_Return_Meta_Response()
+        {
+            var handler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(MetaResponse.ToString())
+                })
+                .Verifiable();
+            var client = new HttpClient(handler.Object);            
+            var transport = new HttpLongPollingTransport(GetOptions(client), new List<IExtension>(), Policy.NoOpAsync());
+            var result = await transport.Request(new List<BayeuxMessage>(), CancellationToken.None).ConfigureAwait(false);
+            Assert.Equal(MetaResponse.ToString(), result.ToString());
+        }
 
-        //[Fact]
-        //public async Task Should_Return_Meta_Response()
-        //{
-        //    var post = MockHttpPost;
-        //    post.Setup(x => x.PostAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
-        //        .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
-        //        {
-        //            Content = new StringContent(MetaResponse.ToString())
-        //        });
+        [Fact]
+        public async Task Should_Notify_Observers_of_Events()
+        {
+            var content = new JArray { MetaResponse, EventResponse, EventResponse };
+            var handler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(content.ToString())
+                })
+                .Verifiable();
+            var client = new HttpClient(handler.Object);
+            var observer = MockObserver;
+            var transport = new HttpLongPollingTransport(GetOptions(client), new List<IExtension>(), Policy.NoOpAsync());
+            transport.Subscribe(observer.Object);
+            await transport.Request(new List<BayeuxMessage>(), CancellationToken.None).ConfigureAwait(false);
+            observer.Verify(x => x.OnNext(It.IsAny<IMessage>()), Times.Exactly(2));
+        }
 
-        //    var transport = new HttpLongPollingTransport(GetOptions(post.Object), new List<IExtension>());
-        //    var result = await transport.Request(new List<BayeuxMessage>(), CancellationToken.None).ConfigureAwait(false);
-        //    Assert.Equal(MetaResponse.ToString(), result.ToString());
-        //}
-
-        //[Fact]
-        //public async Task Should_Notify_Observers_of_Events()
-        //{
-        //    var post = MockHttpPost;
-        //    var observer = MockObserver;
-        //    var content = new JArray {MetaResponse, EventResponse, EventResponse};
-        //    post.Setup(x => x.PostAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
-        //        .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
-        //        {
-        //            Content = new StringContent(content.ToString())
-        //        });
-        //    var transport = new HttpLongPollingTransport(GetOptions(post.Object), new List<IExtension>());
-        //    transport.Subscribe(observer.Object);
-        //    var result = await transport.Request(new List<BayeuxMessage>(), CancellationToken.None).ConfigureAwait(false);
-        //    observer.Verify(x => x.OnNext(It.IsAny<IMessage>()), Times.Exactly(2));
-        //}
-
-        //[Fact]
-        //public async Task Should_Notify_Observers_of_Events_On_Second_Call()
-        //{
-        //    var post = MockHttpPost;
-        //    var observer = MockObserver;
-        //    var content = new JArray { MetaResponse, EventResponse, EventResponse };
-        //    post.SetupSequence(x => x.PostAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
-        //        .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
-        //        {
-        //            Content = new StringContent(content.ToString())
-        //        })
-        //        .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
-        //        {
-        //            Content = new StringContent(content.ToString())
-        //        });
-        //    var transport = new HttpLongPollingTransport(GetOptions(post.Object), new List<IExtension>());
-        //    transport.Subscribe(observer.Object);
-        //    await transport.Request(new List<BayeuxMessage>(), CancellationToken.None).ConfigureAwait(false);
-        //    await transport.Request(new List<BayeuxMessage>(), CancellationToken.None).ConfigureAwait(false);
-        //    observer.Verify(x => x.OnNext(It.IsAny<IMessage>()), Times.Exactly(4));
-        //}
+        [Fact]
+        public async Task Should_Notify_Observers_of_Events_On_Second_Call()
+        {
+            var observer = MockObserver;
+            var content = new JArray { MetaResponse, EventResponse, EventResponse };
+            var handler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handler.Protected()
+                .SetupSequence<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(content.ToString())
+                })
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(content.ToString())
+                });
+            var client = new HttpClient(handler.Object);
+                
+            var transport = new HttpLongPollingTransport(GetOptions(client), new List<IExtension>(), Policy.NoOpAsync());
+            transport.Subscribe(observer.Object);
+            await transport.Request(new List<BayeuxMessage>(), CancellationToken.None).ConfigureAwait(false);
+            await transport.Request(new List<BayeuxMessage>(), CancellationToken.None).ConfigureAwait(false);
+            observer.Verify(x => x.OnNext(It.IsAny<IMessage>()), Times.Exactly(4));
+        }
 
         [Fact]
         public async Task Should_Throw_BayeuxProtocolException_When_Message_Missing_Channel()
@@ -108,8 +121,6 @@ namespace FinancialHq.Bayeux.Tests.Unit.Client.Connectivity
             await Assert.ThrowsAsync<BayeuxProtocolException>(async () =>
                  await transport.Request(new List<BayeuxMessage>(), CancellationToken.None).ConfigureAwait(false)).ConfigureAwait(false);
         }
-
-        private Mock<HttpMessageHandler> MockHttpMessageHandler => new Mock<HttpMessageHandler>(MockBehavior.Strict);
 
         private Mock<IObserver<IMessage>> MockObserver => new Mock<IObserver<IMessage>>();
 
